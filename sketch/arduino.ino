c635d95d88ca8ddf663974a86a78e635856bb17c
Timothy O'Keefe <tokeefe@fas.harvard.edu>
2012-07-15 17:36:54 -0400

    updated sketch

--- sketch/arduino.ino
+++ sketch/arduino.ino
@@ -3,23 +3,27 @@
 int pin = 0;
 char out[16];
 boolean start = false;
+unsigned long int t0;
 
 void setup()
 {
   Serial.begin(115200);
+  Serial.flush();
+  t0 = millis();
 }
 
 void loop()
 {
-  if(Serial.available())
-  {
-    if(Serial.read() == 255)
-      start = true;
-  }
-  
   if(start)
   {
     sprintf(out, "%i,%u\n", analogRead(pin), millis());
     Serial.print(out);
+    return;
+  }
+  
+  if(Serial.available() > 0)
+  {
+    start = true;
+    Serial.println(255);
   }
 }
